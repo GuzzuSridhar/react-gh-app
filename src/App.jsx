996c3b40483cca75345ed96cb3f939d662f8090a
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2026-07-14 11:54:14 +0530

    append vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import Welcome from "./components/Welcome"
+import About from "./components/About"
 function App() {
+  const basename =
+    import.meta.env.PROD ? "/react-gh-app" : "";
+
   return (
     <>
 
-      <BrowserRouter basename="/react-gh-app">
+      <BrowserRouter basename={basename}>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/welcome">Welcome</Link>
+          <Link to="/about">About</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/welcome" element={<Welcome />} />
-          <Route path="/about" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
 
         </Routes>
       </BrowserRouter>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/welcome">Welcome</Link>
-        <Link to="/about">About</Link>
-      </nav>
+
 
     </>
   )
